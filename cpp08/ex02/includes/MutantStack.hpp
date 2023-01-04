da14a2eb0d4f1f3d0ce397d6c1433a3239f0aedf
Vincent DURIEZ <vduriez@e2r13p6.clusters.42paris.fr>
2023-01-05 00:07:04 +0100

    modified to give better explanations

--- cpp08/ex02/includes/MutantStack.hpp
+++ cpp08/ex02/includes/MutantStack.hpp
@@ -17,15 +17,15 @@ class MutantStack : public std::stack<T>
 		MutantStack<T>(MutantStack const & parent) : std::stack<T>(parent) {}
 		virtual ~MutantStack<T>(void) {}
 
-		typedef typename std::stack< T >::container_type::iterator iterator;
+		typedef typename std::stack<T>::container_type::iterator iterator;
 		iterator begin()
 		{
-			return (std::stack< T >::c.begin());
+			return (std::stack<T>::c.begin());
 		}
 
 		iterator end()
 		{
-			return (std::stack< T >::c.end());
+			return (std::stack<T>::c.end());
 		}
 		
 		MutantStack<T> & operator=(MutantStack const & rhs)
